a6f9b66ca9328404ea03b2b831289b0c45cd3207
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-01-14 09:55:20 +0530

    Update Day8CodeShare.cs

--- Day8CodeShare.cs
+++ Day8CodeShare.cs
@@ -62,4 +62,55 @@ namespace GenericsDemo
 
     }
 }
+now i am editing the code i can comment now as i am using helper1 generic method 
+
+namespace GenericsDemo
+{
+    
+    class Program
+    {
+
+        //public static void swap(ref int x, ref int y)
+        //{
+        //    int temp;
+        //    temp = x;
+        //    x = y;
+        //    y = temp;
+        //}
+
+        //public static void swap(ref DateTime x, ref DateTime y)
+        //{
+        //    DateTime temp;
+        //    temp = x;
+        //    x = y;
+        //    y = temp;
+        //}
+        //public static void swap(ref string x, ref string y)
+        //{
+        //    string temp;
+        //    temp = x;
+        //    x = y;
+        //    y = temp;
+        //}
+      static void Main(string[] args)
+        {
+            DateTime date1 = DateTime.Now;
+            DateTime date2 = DateTime.Now.AddDays(2);
+            Console.WriteLine("\nBefore swapping");
+            Console.WriteLine($"Date1:{date1} \nDate2:{date2}");
+          //  swap(ref date1, ref date2);
+            Helper1.swap<DateTime>(ref date1, ref date2);
+            Console.WriteLine("\nAfter swapping");
+            Console.WriteLine($"Date1:{date1} \nDate2:{date2}");
+
+            int a = 10, b = 20;
+            Console.WriteLine("\nBefore swapping");
+            Console.WriteLine($"a:{a} \nb:{b}");
+            Helper1.swap<int>(ref a, ref b);
+            Console.WriteLine("\nAfter swapping");
+            Console.WriteLine($"a:{a} \nb:{b}");
+            Console.ReadLine();
+        }
+    }
+}
 
