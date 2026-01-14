b10cbe978ea0eb91c5926ee632db44e79ae2e981
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-01-14 11:03:46 +0530

    Update Day8CodeShare.cs

--- Day8CodeShare.cs
+++ Day8CodeShare.cs
@@ -457,3 +457,63 @@ namespace delegatedemo1
     }
 }
 
+
+
+anaonymous execution :
+----------------------
+A function which will not have any  name is called anomymous function and that function can be executed using delegate
+  reference so let us see that one 
+
+namespace anonoumuusingdelegate
+{
+    internal class Program
+    {
+        //public static void add(int x, int y)
+        //{
+        //    Console.WriteLine($"The sum is :{x + y}");
+        //}
+        //public static int substract(int x, int y)
+        //{
+        //    return x - y;
+        //}
+        //public static int multiply(int x, int y)
+        //{
+        //    return x * y;
+        //}
+        //public static void divide(int x, int y)
+        //{
+        //    Console.WriteLine($"The division is :{x / y}");
+
+        //}
+
+        public delegate void mydelegate1(int a, int b);
+        public delegate int mydelegate2(int a, int b);
+    
+        static void Main(string[] args)
+        {
+            mydelegate1 m1 = delegate (int x, int y)
+            {
+                Console.WriteLine($"The sum is :{x + y}");
+            };
+            m1 += delegate (int x, int y)
+            {
+                Console.WriteLine($"The division is :{x / y}");
+
+            };
+            m1(12, 4);
+            mydelegate2 m2 = delegate (int x, int y)
+            {
+                return x - y;
+            };
+            Console.WriteLine($"subsctraction is :{m2.Invoke(12, 7)}");
+            m2 += delegate (int x, int y)
+            {
+                return x * y;
+            };
+            Console.WriteLine($"multiplication  is :{m2.Invoke(12, 7)}");
+            Console.ReadLine();
+
+        }
+    }
+}
+
