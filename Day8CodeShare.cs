4463a1b25a942f18441c71c580ae5714a7a55ba1
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-01-14 10:13:17 +0530

    Update Day8CodeShare.cs

--- Day8CodeShare.cs
+++ Day8CodeShare.cs
@@ -146,4 +146,61 @@ namespace GenericsDemo
     }
 }
 
+main method code 
+------------------
+namespace GenericsDemo
+{
+    
+    class Program
+    {
+
+        //public static void swap(ref int x, ref int y)
+        //{
+        //    int temp;
+        //    temp = x;
+        //    x = y;
+        //    y = temp;
+        //}
+
+        //public static void swap(ref DateTime x, ref DateTime y)
+        //{
+        //    DateTime temp;
+        //    temp = x;
+        //    x = y;
+        //    y = temp;
+        //}
+        //public static void swap(ref string x, ref string y)
+        //{
+        //    string temp;
+        //    temp = x;
+        //    x = y;
+        //    y = temp;
+        //}
+      static void Main(string[] args)
+        {
+            DateTime date1 = DateTime.Now;
+            DateTime date2 = DateTime.Now.AddDays(2);
+            Console.WriteLine("\nBefore swapping");
+            Console.WriteLine($"Date1:{date1} \nDate2:{date2}");
+          //  swap(ref date1, ref date2);
+            Helper1.swap<DateTime>(ref date1, ref date2);
+            Console.WriteLine("\nAfter swapping");
+            Console.WriteLine($"Date1:{date1} \nDate2:{date2}");
+
+            int a = 10, b = 20;
+            Console.WriteLine("\nBefore swapping");
+            Console.WriteLine($"a:{a} \nb:{b}");
+            Helper1.swap<int>(ref a, ref b);
+            Console.WriteLine("\nAfter swapping");
+            Console.WriteLine($"a:{a} \nb:{b}");
+
+            double d1 = 345.67;
+            double d2 = 378.89;
+            Console.WriteLine($"The double sum are :{Helper1.add(d1, d2)}");
+            Console.WriteLine($"The int sum are :{Helper1.add(a, b)}");
+
+            Console.ReadLine();
+        }
+    }
+}
 
