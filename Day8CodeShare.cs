d625a7a755cd28e1de7a52c6b8dd6e027ec91a0d
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-01-14 14:13:05 +0530

    Update Day8CodeShare.cs

--- Day8CodeShare.cs
+++ Day8CodeShare.cs
@@ -881,6 +881,50 @@ namespace setandgetmethodsdemo
     }
 }
 
+Next i am using set and get methods 
+-----------------------------------
+namespace setandgetmethodsdemo
+{
+    class Customer
+    {
+        private int _cid=-1;
+        public int ID
+        {
+            set
+            {
+                _cid = value;
+            }
+            get
+            {
+                return _cid;
+            }
+        }
+        private string _Cname=string.Empty;
+        public string CustomerName
+        {
+            set
+            {
+                _Cname = value;
+            }
+            get
+            {
+                return _Cname;
+            }
+        }
+       
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer obj = new Customer();
+            obj.ID = 101;
+            obj.CustomerName = "ravi";
+            Console.WriteLine($"{obj.ID}--{obj.CustomerName}");
+            Console.ReadLine();
+        }
+    }
+}
 
 
 	
