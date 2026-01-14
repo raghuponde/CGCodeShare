82d88813dae95eed4460ab10581d9b7d241578ea
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-01-14 14:59:19 +0530

    Update Day8CodeShare.cs

--- Day8CodeShare.cs
+++ Day8CodeShare.cs
@@ -1062,3 +1062,46 @@ namespace Enumdemo
         }
     }
 }
+
+
+final code of enum
+------------------
+	namespace Enumdemo
+{
+    class Employee
+    {
+        public int id { set; get; }
+        public string Name { set; get; }
+      
+
+         public EmployeeType Type { get; set; }
+
+
+    }
+    enum EmployeeType
+    {
+        Fulltime, partime, contract
+    }
+
+    internal class Program
+    {
+       
+        enum weekdays { sun=10,mon,tue,wed,thu=100,fri,sat};
+
+        static void Main(string[] args)
+        {
+           
+          Console.WriteLine($"{(int)weekdays.fri}");
+            Console.WriteLine($"{(int)weekdays.mon}");
+            Employee objemp = new Employee();
+            objemp.id = 101;
+            objemp.Name = "ravi";
+            objemp.Type = EmployeeType.Fulltime;
+
+            Console.WriteLine($"{objemp.id}--{objemp.Name}--{objemp.Type}");
+            Console.ReadLine();
+        }
+    }
+}
+
+	
