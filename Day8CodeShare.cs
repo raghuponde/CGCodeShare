6f8675cbe7ff20ea36c19f2ee53c7e4295cc957a
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-01-14 09:40:39 +0530

    Update Day8CodeShare.cs

--- Day8CodeShare.cs
+++ Day8CodeShare.cs
@@ -1,1 +1,35 @@
 
+namespace GenericsDemo
+{
+    
+    class Program
+    {
+
+        public static void swap(ref int x, ref int y)
+        {
+            int temp;
+            temp = x;
+            x = y;
+            y = temp;
+        }
+
+        public static void swap(ref DateTime x,ref DateTime y)
+        {
+            DateTime temp;
+            temp = x;
+            x = y;
+            y = temp;
+        }
+        public static void swap(ref string x,ref string y)
+        {
+            string temp;
+            temp = x;
+            x = y;
+            y = temp;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
