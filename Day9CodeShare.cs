94b6b3ac75782b77539b66691f27a84718a20c79
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-01-15 10:47:46 +0530

    Update Day9CodeShare.cs

--- Day9CodeShare.cs
+++ Day9CodeShare.cs
@@ -368,3 +368,47 @@ namespace Tryparsedemo
     }
 }
 
+Next updated code 
+-----------------
+
+namespace Tryparsedemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // tryparsedemo();
+            parsedemo();
+
+            Console.ReadLine();
+        }
+
+        private static void parsedemo()
+        {
+            Console.WriteLine("Enter Product Price ");
+            decimal price = decimal.Parse(Console.ReadLine());
+            //here if u enter wrong value exception will be thrown
+            Console.WriteLine($"Product Price is : {price}");
+        }
+
+        private static void tryparsedemo()
+        {
+
+            Console.WriteLine("Enter Product Price ");
+            string input = Console.ReadLine();
+            bool isvalid = decimal.TryParse(input, out decimal price);
+            // if it becomes true then it will store its value in price variable
+            // try parse return type is boolean
+            // for any type i can do tryparse ,here this will not throw exception 
+            // becasue i am handing in else block the logic 
+            if (isvalid)
+            {
+                Console.WriteLine($"Product Price is : {price}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Price Input please enter numeric value");
+            }
+        }
+    }
+}
