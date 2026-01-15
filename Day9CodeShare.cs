304122214ec8827f1f2b9eb50446ed6b13c112e0
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-01-15 09:52:38 +0530

    Update Day9CodeShare.cs

--- Day9CodeShare.cs
+++ Day9CodeShare.cs
@@ -1,1 +1,45 @@
 
+namespace paramsdemo
+{
+    class Employee
+    { 
+       //public void tsal(int sal,int bonus,int allowances)
+       // {
+       //     Console.WriteLine($"Total Salary is:{(sal + bonus + allowances)} " );
+       // }
+
+       // public void tsal(int sal, int bonus, int allowances,int hraallowences)
+       // {
+       //     Console.WriteLine($"Total Salary is:{(sal + bonus + allowances+hraallowences)} ");
+       // }
+
+       // public void tsal(int sal, int bonus)
+       // {
+       //     Console.WriteLine($"Total Salary is:{(sal + bonus) } ");
+       // }
+
+        public void tsal(params int[] sal)
+        {
+            int ts = 0;
+            for(int i=0;i<sal.Length;i++)
+            {
+                ts=ts+sal[i];
+            }
+            Console.WriteLine($"Total Salary is:{ts} ");
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee empobj=new Employee();
+            empobj.tsal(50000, 10000, 5000);
+            empobj.tsal(50000, 10000, 5000, 8000);
+            empobj.tsal(50000, 10000);
+            empobj.tsal(50000, 10000, 5000, 8000, 2000, 3000);
+
+
+        }
+    }
+}
