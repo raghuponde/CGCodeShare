a029020e1994883b4c415e553c258e3cc5b90f9d
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-01-15 11:40:23 +0530

    Update Day9CodeShare.cs

--- Day9CodeShare.cs
+++ Day9CodeShare.cs
@@ -587,4 +587,40 @@ if (m != null)
     m.ApproveLeave();
 }
 
+Partial class 
+-------------
 
+namespace partialclassdemo
+{
+
+    public partial class Employee
+    {
+        public int Empid { get; set; }
+        public string EmpName { get; set; }
+
+        public Employee(int empid,string empname)
+        {
+            this.Empid = empid;
+            this.EmpName = empname;
+        }
+    }
+
+    public partial class Employee
+    {
+        public void DisplayEmployee()
+        {
+            Console.WriteLine($"{Empid}--{EmpName}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee empobj = new Employee(101, "kiran");
+            empobj.DisplayEmployee();
+            Console.ReadLine();
+        }
+    }
+}
+
+    
