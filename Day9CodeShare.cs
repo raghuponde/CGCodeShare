7b811c08a6cb71f5bbe8e7d40931d8277cc38f10
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-01-15 11:05:54 +0530

    Update Day9CodeShare.cs

--- Day9CodeShare.cs
+++ Day9CodeShare.cs
@@ -494,5 +494,74 @@ namespace IsAsDemo
     }
 }
 
+final code 
+----------
+namespace IsAsDemo
+{
+    class Employee
+    {
+         public string Name { get; set; }
+        public Employee(string name)
+        {
+            Name = name;
+        }
+
+    }
+    class Manager : Employee
+    {
+        public Manager(string name) : base(name)
+        {
+        }
+        public void ApproveLeave()
+        {
+            Console.WriteLine($" {Name} approved leave request");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee("Alice");
+            Employee emp2 = new Manager("Bob");
+            // using is 
+            if ((emp1 is Manager))
+            {
+                Console.WriteLine("emp1 is manager");
+            }
+            else
+            {
+                Console.WriteLine("emp1 is not manager");
+            }
+            if (emp2 is Manager)
+            {
+                Console.WriteLine("emp2 is manager");
+            }
+            else
+            {
+                Console.WriteLine("emp2 is not manager");
+            }
+            //now using as ..for safe typecastng
+            Manager mgr1=emp1 as Manager;//emp1 isnot a manager so mgr1 becomes null
+            Manager mgr2=emp2 as Manager;
+            if (mgr1 != null)
+            {
+                mgr1.ApproveLeave();
+            }
+            else
+            {
+                Console.WriteLine("emp1 is not a manager, cannot approve leave");
+            }
+            if (mgr2 != null)
+            {
+                mgr2.ApproveLeave();
+            }
+            else
+            {
+                Console.WriteLine("emp2 is not a manager, cannot approve leave");
+            }
+            Console.ReadLine();
+        }
+    }
+}
 
 
