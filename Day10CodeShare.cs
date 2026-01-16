9fd7e2469c0f5b889f1ae37d70fc5c0430dd7d65
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-01-16 09:39:41 +0530

    Update Day10CodeShare.cs

--- Day10CodeShare.cs
+++ Day10CodeShare.cs
@@ -1,1 +1,24 @@
+List Demo of geenric 
+--------------------
+namespace GenericListDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            numbers.Add(1);
+            numbers.Add(29);
+            numbers.Add(45);
+
+            List<string> names = new List<string>()
+            { 
+            "ravi",
+            "sudha",
+            "mohan"
+            
+            };
+        }
+    }
+}
 
