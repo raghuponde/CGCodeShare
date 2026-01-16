541df2e6b2082f441fbbecb77f29e779962bf5c1
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-01-16 15:10:39 +0530

    Update Day10CodeShare.cs

--- Day10CodeShare.cs
+++ Day10CodeShare.cs
@@ -569,3 +569,79 @@ namespace IComparabledemo
 }
 I cannot sort employees because is confused on what bais i have to sorrt 
 
+
+namespace IComparabledemo
+{
+    class Employee:IComparable
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public decimal Salary { get; set; }
+
+        public Employee(string name, int id, decimal salary)
+        {
+            Name = name;
+            Id = id;
+            Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} (ID: {Id}, Salary: {Salary:C})";
+        }
+
+        public int CompareTo(object? obj)
+        {
+            Employee otheremployee = obj as Employee;
+            decimal thissalary = this.Salary;
+            decimal otheremployesalary = otheremployee.Salary;
+            if (thissalary < otheremployesalary)
+            {
+                return -1;
+            }
+            else if (thissalary > otheremployesalary)
+            {
+                return 1;
+            }
+            else
+                return 0;
+
+        }
+
+        
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 12, 67, 8,
+                2, 78, 43, 22, 90, 129 };
+            // primitive data types 
+
+            numbers.Sort();
+            foreach (int i in numbers)
+            {
+                Console.Write($"{i}  ");
+            }
+
+            List<Employee> employees = new List<Employee>()
+            {
+                new Employee("Jane", 102, 75000m),
+               new Employee("John", 101, 90000m),
+                new Employee("Mike", 103, 65000m)
+            };
+
+            employees.Sort();
+            
+            Console.WriteLine("\n");
+            foreach (Employee employee in employees)
+            {
+                Console.WriteLine(employee);
+            }
+
+
+        }
+    }
+}
+
+
