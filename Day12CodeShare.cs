81f692b4db6e6b1bd664f37aaed46d6796011483
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-01-19 11:08:47 +0530

    Update Day12CodeShare.cs

--- Day12CodeShare.cs
+++ Day12CodeShare.cs
@@ -10,10 +10,28 @@ namespace FileHandlinginWinForms
 
         private void button1_Click(object sender, EventArgs e)
         {
-          DialogResult res=  colorDialog1.ShowDialog();
+            DialogResult res = colorDialog1.ShowDialog();
             if (res == DialogResult.OK)
             {
-                textBox1.ForeColor = colorDialog1.Color; 
+                textBox1.ForeColor = colorDialog1.Color;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult res = colorDialog1.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                textBox1.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+          DialogResult res=  fontDialog1.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                textBox1.Font = fontDialog1.Font;
             }
         }
     }
