146716191dd7103e205745fe760d1ef94dd84b1e
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-01-19 11:05:43 +0530

    Update Day12CodeShare.cs

--- Day12CodeShare.cs
+++ Day12CodeShare.cs
@@ -1,1 +1,20 @@
 
+namespace FileHandlinginWinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          DialogResult res=  colorDialog1.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                textBox1.ForeColor = colorDialog1.Color; 
+            }
+        }
+    }
+}
