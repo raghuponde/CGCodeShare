079087d55ad3810ae55e231332750463dd2c99c5
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-01-21 11:15:46 +0530

    Update Day13CodeShare.cs

--- Day13CodeShare.cs
+++ Day13CodeShare.cs
@@ -12,8 +12,10 @@ add three text boxes and one button for serilize and deserilize like that and co
 
 using System.Collections;
 using System.Runtime.Serialization.Formatters.Soap;
+using System.Runtime.Serialization.Formatters.Binary;
 namespace serilization
 {
+
     public partial class Form1 : Form
     {
         public Form1()
@@ -36,6 +38,50 @@ namespace serilization
             {
                 ss.Serialize(fs, obj);
             }
+        }
+
+
+
+
+
+        [Serializable]
+        class Customer
+        {
+            public int CustomerID { set; get; }
+            public string CustomerName { set; get; }
+            public string City { set; get; }
+        }
+        private const string filename2 = "data.dat";
+        FileStream fs;
+        System.Runtime.Serialization.Formatters.Binary.BinaryFormatter formatter;
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            Customer c1 = new Customer();
+            c1.CustomerID = Convert.ToInt32(textBox2.Text);
+            c1.CustomerName = textBox3.Text;
+            c1.City = textBox4.Text;
+            fs = File.Create(filename2);
+            formatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+            formatter.Serialize(fs, c1);
+            fs.Close();
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            fs = File.OpenRead(filename2);
+            Customer c2 = (Customer)formatter.Deserialize(fs);
+            textBox2.Text = c2.CustomerID.ToString();
+            textBox3.Text = c2.CustomerName;
+            textBox4.Text = c2.City;
+            fs.Close();
+
         }
     }
 }
