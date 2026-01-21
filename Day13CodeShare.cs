2c62018f65959541e8aadcb3cd4fe3b63987e0cd
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-01-21 09:58:29 +0530

    Update Day13CodeShare.cs

--- Day13CodeShare.cs
+++ Day13CodeShare.cs
@@ -1,1 +1,10 @@
+Serilization :
+**************
+For an application all its data is stored in memory what if u want to store that data somewhere else or transport it to some process or to some other location for that u have to convert that data into 
+binary ,xml or some other format to store it or transfer so we can say saving the state of an object  permenantly in one format is called as serilization .
 
+open one windows based application and put one button and one text box as well 
+addd reference to dll file System.Runtime.Serialization.Formatters.Binary;and 
+System.Runtime.Serialization.Formatters.Soap; and as usual include namespaces 
+
+add three text boxes and one button for serilize and deserilize like that and code is below 
