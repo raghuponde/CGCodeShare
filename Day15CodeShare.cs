16325840175b9be557924aa4b0185f9cf49dbb5b
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-01-23 10:19:52 +0530

    Update Day15CodeShare.cs

--- Day15CodeShare.cs
+++ Day15CodeShare.cs
@@ -61,3 +61,31 @@ namespace RegexDemo
 
 Version 3 (Final version ) 
 ----------------------------
+using System.Text.RegularExpressions;
+
+namespace RegexDemo
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            var pattern = Console.ReadLine();
+            var subject = Console.ReadLine();
+
+            var regex = new Regex(pattern);
+            //once u make the instance of regex u cannot change it means if u want to make
+            //use of different pattern another instance of regex has to be created .
+            var match= regex.Match(subject);
+            //if pattern is found in the subject it returns boolean value as true ..
+           while(match.Success)
+            {
+                Console.WriteLine($"{match.Success}--@{match.Index}--{match.Length}");
+                match = match.NextMatch();
+            }
+
+                Console.ReadLine();
+
+        }
+    }
+}
