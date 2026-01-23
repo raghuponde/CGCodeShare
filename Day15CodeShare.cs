6f47e613adbf073b66d2e08ca2f555977ad57ee2
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-01-23 09:58:27 +0530

    Update Day15CodeShare.cs

--- Day15CodeShare.cs
+++ Day15CodeShare.cs
@@ -1,1 +1,25 @@
+version 1 of program 
+---------------------
+using System.Text.RegularExpressions;
 
+namespace RegexDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var pattern = Console.ReadLine();
+            var subject = Console.ReadLine();
+
+            var regex = new Regex(pattern);
+            //once u make the instance of regex u cannot change it means if u want to make
+            //use of different pattern another instance of regex has to be created .
+            var match= regex.Match(subject);
+            //if pattern is found in the subject it returns boolean value as true ..
+
+            Console.WriteLine(match.Success);
+            Console.ReadLine();
+
+        }
+    }
+}
