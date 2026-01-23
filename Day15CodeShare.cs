ed01ed62672ea62d196063ba7bb892d2c2af32ba
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-01-23 11:55:01 +0530

    Update Day15CodeShare.cs

--- Day15CodeShare.cs
+++ Day15CodeShare.cs
@@ -278,5 +278,33 @@ namespace hai
     }
 }
 
+ 2)Local Function 
+ ********************
+ namespace LocalFunctionsDemo
+{
+    internal class Program
+    {
+        public int CaluclateSomething (int a, int b)
+        {
+            int sum = a + b;
+            int difference = a - b;
+            return sum * difference;
+            int Sum(int x,int y)
+            {
+                return x + y;
+            }
+            int Difference(int x,int y)
+            {
+                return x - y; 
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program pp=new Program();
+            Console.WriteLine($"Result :{pp.CaluclateSomething(6, 4)}");
+            Console.ReadLine();
+        }
+    }
+}
 
 
