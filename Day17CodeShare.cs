e3eb1f2c428f142def130b589535267543fe390a
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-01-27 14:31:58 +0530

    Update Day17CodeShare.cs

--- Day17CodeShare.cs
+++ Day17CodeShare.cs
@@ -481,6 +481,47 @@ namespace ThreadDemo1
     }
 }
 
+another version without delegate 
+   -----------------------------
+   using System.Threading;
+
+namespace ThreadDemo1
+{
+    internal class Program
+    {
+
+        public static void Func1()
+        {
+            for(int i=0;i<10;i++)
+            {
+                Console.WriteLine($"Func 1 write {i}");
+            }
+        }
+        public static void Func2()
+        {
+            for (int i = 10; i >0; i--)
+            {
+                Console.WriteLine($"Func 2 write {i}");
+            }
+        }
+       // public delegate void ThreadStart1();
+        // 
+        static void Main(string[] args)
+        {
+            //ThreadStart t1 = new ThreadStart(Func1);
+            //ThreadStart t2 = new ThreadStart(Func2);
+            Thread first = new Thread(Func1);
+          Thread second = new Thread(Func2);
+            first.Priority = ThreadPriority.Lowest;
+            second.Priority = ThreadPriority.Highest;
+
+            first.Start();
+            second.Start();
+           
+            Console.ReadLine();
+        }
+    }
+}
 
 
 The above program with run sequentitaly means as per the order the above program will run now i want to make above programas Thread based program
