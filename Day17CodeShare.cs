2a5e3fb684565d9381125b89436d2cfc9655d26a
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-01-27 15:44:13 +0530

    Update Day17CodeShare.cs

--- Day17CodeShare.cs
+++ Day17CodeShare.cs
@@ -830,4 +830,86 @@ namespace AsyncAwaitDemo
         }
     }
 }
+now using versions 
+---------------------
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Before dealy";
+            //   for (int i = 0; i < 10000; i++) ;
+            Task.Delay(10000).Wait();
+            label2.Text = "after delay";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            label2.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("say hello..");
+        }
+    }
+}
+second version 
+----------------
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Before dealy";
+            //   for (int i = 0; i < 10000; i++) ;
+          await  Task.Delay(10000);
+            label2.Text = "after delay";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            label2.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("say hello..");
+        }
+    }
+}
 
