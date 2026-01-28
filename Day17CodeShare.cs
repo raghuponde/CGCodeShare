359f855e5c43148bc8b719e7518e3bff84ed996e
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-01-28 10:47:14 +0530

    Update Day17CodeShare.cs

--- Day17CodeShare.cs
+++ Day17CodeShare.cs
@@ -1,4 +1,3 @@
-
 create database CG
 use CG;
 create table student(studid int primary key,studname varchar(30),
@@ -16,6 +15,29 @@ insert into student(studid,studname) values(103,'suresh')
 --multiple insert 
 insert into student values(104,'kiran','delhi'),(105,'sita','noida'),
 (106,'shnathi','hyderabad')
+
+--updating single column in a single row (cant update primary key)
+
+update student set studname='joseph' where studid=102;
+
+--updating multiple columns in a single row 
+update student set studname='david',location='kanyakumari' where studid=104;
+
+--updating multiple rows in a single column 
+update student set location='calcutta' where studid in(102,104,105);
+
+--updating multiple rows in multiple columsn is not possible 
+
+--deleting single row 
+delete from student where studid=105;
+--deleting multiple rows
+delete from student where studid in (102,104)
+--deleting all rows 
+delete from student;
+--delete student ; thi is also okay 
+
+
+
 
 
 
