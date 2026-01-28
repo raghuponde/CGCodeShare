34ffc435216b8397c500c1d8c20d22aa890f3aae
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-01-28 10:39:32 +0530

    Update Day17CodeShare.cs

--- Day17CodeShare.cs
+++ Day17CodeShare.cs
@@ -1,1 +1,22 @@
 
+create database CG
+use CG;
+create table student(studid int primary key,studname varchar(30),
+location varchar(50))
+
+--normal insert 
+insert into student values(101,'ravi','chennai');
+
+select * from student
+
+--inserting as per my order 
+insert into student(location,studid,studname)values('bangalore',102,'mahesh')
+--partial insert 
+insert into student(studid,studname) values(103,'suresh')
+--multiple insert 
+insert into student values(104,'kiran','delhi'),(105,'sita','noida'),
+(106,'shnathi','hyderabad')
+
+
+
+
