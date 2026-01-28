3eb9e5edee7181ea05fdcb0c4e89d4b9a3826292
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-01-28 11:25:33 +0530

    Update Day17CodeShare.cs

--- Day17CodeShare.cs
+++ Day17CodeShare.cs
@@ -59,5 +59,33 @@ insert into empinfo values('ayyapa');
 -- so u can see log are mainted when u delete command is used but 
 -- for truncate it is not maintained it starts from first 
 
+--Constraints 
+--constraint <constraintname> typeofconstraint(collist)
+--1)null or not null this is columns level constraint can be applied only beside column
+--only 
+create table demo1(id int not null,fname varchar(30),mname varchar(30) null,lname
+varchar(40))
+-- here not null means dont forget the value but duplicates are allowed 
+insert into demo1 values(null,null,null,null);--err
+insert into demo1 values(101,null,null,null)
+insert into demo1 values(101,null,null,null)-- duplicates allwed 
+insert into demo1 values(101,null,null,'janaki')
+
+-- you cannot use this null as table level constraint 
+-- it is used as column level only
+
+--2)unique constraint 
+--here u cannot enter duplicate values but u can enter null values but how many  nulls
+-- depends on system or type of dataabse u are using
+-- can be applied both as column level and table level 
+create table demo2(id int unique,fname varchar(30) not null,mname varchar(30) null,lname
+varchar(40),constraint uk1 unique(mname,lname))
+
+insert into demo2 values(101,'kiran',null,'kumar');--fine
+insert into demo2 values(102,'kiran',null,'das')--fine
+insert into demo2 values(102,'kiran',null,'das')--error
+insert into demo2 values (103,'kiran',null,'kishore')--fine
+insert into demo2 values (null,'kiran',null,'kishore1')--fine
+insert into demo2 values (null,'kiran',null,'kishore2') --error
 
 
