afe3cf095e1abd4869ad29d21e371e4ee00aaabd
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-01-29 09:32:32 +0530

    Update Day18CodeShare.cs

--- Day18CodeShare.cs
+++ Day18CodeShare.cs
@@ -1,1 +1,8 @@
 
+referential integrity constratint which is also called foreign key ...
+---------------------------------------------------------------------
+Definition : Two Tables are said to related to each other if they are having one common column between them and that common column should act as primary key in the master table and the 
+  same column will be called foreign key in child .
+
+The table which i create first is the master table  which is not depending on any table for is exsistence 
+but the child table will depend on master table it should contain in the foreign key only the values of master table .
