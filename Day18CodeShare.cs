216ceaa6e048665c66cebd4cddafbdd9d4a38eb5
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-01-29 12:07:20 +0530

    Update Day18CodeShare.cs

--- Day18CodeShare.cs
+++ Day18CodeShare.cs
@@ -211,3 +211,20 @@ e1.worksin=d1.deptid inner join location l1 on e1.empid=l1.empid;
 --version 2 
 select e1.empname from emp3 e1 inner join dept3 d1 on
 e1.worksin=d1.deptid inner join location l1 on e1.empid=l1.empid;
+
+-- give me all the employees who got dept but not location 
+--version 1
+select e1.empname,d1.deptname,l1.locname from emp3 e1 inner join dept3 d1 on
+e1.worksin=d1.deptid left join location l1 on e1.empid=l1.empid;
+
+--version 2 
+select e1.empname,d1.deptname,l1.locname from emp3 e1 inner join dept3 d1 on
+e1.worksin=d1.deptid left join location l1 on e1.empid=l1.empid where l1.locname is null;
+
+--version 3 
+
+select e1.empname from emp3 e1 inner join dept3 d1 on
+e1.worksin=d1.deptid left join location l1 on e1.empid=l1.empid where l1.locname is null;
+
+
+
