9fe147f7fd85d913a7a8db68099d50efb58662d0
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-01-30 09:36:28 +0530

    Update Day19CodeShare.cs

--- Day19CodeShare.cs
+++ Day19CodeShare.cs
@@ -1,1 +1,17 @@
 
+SELF JOIN 
+----------
+self join means joining the table with itself now here only one table is there okay 
+and in this table only some body is manaager for some body so as per question i had done self join 
+for the same table two objects i created .
+
+
+create table Employee(id int primary key ,
+name varchar(50) not null,
+managerid int );
+
+
+insert into employee values(1,'mike',3),(2,'David',3),(3,'Roger',null),(4,'Marry',2),(5,'Joseph',2),
+(7,'Ben',2);
+
+select * from employee;
