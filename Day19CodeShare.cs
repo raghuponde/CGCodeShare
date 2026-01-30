a0de83ebde661e58709944ae77e68c5ad4f6e889
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-01-30 09:44:03 +0530

    Update Day19CodeShare.cs

--- Day19CodeShare.cs
+++ Day19CodeShare.cs
@@ -15,3 +15,10 @@ insert into employee values(1,'mike',3),(2,'David',3),(3,'Roger',null),(4,'Marry
 (7,'Ben',2);
 
 select * from employee;
+
+-- give me all the employees with the names and thier corresponding manager names 
+
+select e1.name as "empname" ,e2.name as "Manager" from Employee e1 inner join Employee e2 on 
+e1.managerid=e2.id;
+
+
