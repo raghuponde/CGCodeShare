c3e6163fee9128d986bcf0e7fa291548ab45e050
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-02-02 09:49:57 +0530

    Update Day21CodeShare.cs

--- Day21CodeShare.cs
+++ Day21CodeShare.cs
@@ -1,1 +1,29 @@
 
+co related subquery 
+----------------------
+co related subquery means it will depend on parent query for its exsistence it cannot execute individually like a
+ normal subquery some scenarios or some requirement will force us to implement co realted sub query and here 
+the task which u are doing in co related subquery can also be done using joins it depends how much deep u can think 
+
+
+check slide for syntax 
+
+
+create table Products(productid int primary key ,prodname varchar(40),Description varchar(100))
+
+insert into products values(1,'TV','52 inch black color lcd TV ');
+insert into products values(2,'Laptop','very thin silver predator latop  ');
+insert into products values(3,'Desktop','HP high performance dektop');
+
+select * from products;
+
+create table Productsales(salesid int primary key ,productid int ,UserPrice int ,quantitysold int ,
+constraint ssk foreign key (productid) references Products(productid))
+
+insert into Productsales values(1,3,450,5);
+insert into Productsales values(2,2,250,7);
+insert into Productsales values(3,3,450,4);
+insert into Productsales values(4,3,450,9);
+
+select * from Products;
+select * from Productsales;
