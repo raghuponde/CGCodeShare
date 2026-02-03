3420f5454304cb3483891315d8e9306b5e54b22d
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-02-03 11:16:16 +0530

    Update Day22CodeShare.cs

--- Day22CodeShare.cs
+++ Day22CodeShare.cs
@@ -267,3 +267,21 @@ print @name1;
 end
 
 exec selectstud 101 
+
+ 
+right click on database and say restore database 
+
+click device and three dots 
+
+then say add 
+
+copy the path from the above highlighted text box into the notepad only the path u copy 
+
+C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\Backup  
+
+after copying the path close all widows 
+
+and in that path u put these two .bak files 
+
+then again follow the same steps and select the database say  ok and then in window select the file path and say ok and again ok 
+
