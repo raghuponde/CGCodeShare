6c58d189e86b964149cf60377124b1ff6e1f53e4
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-02-05 09:41:18 +0530

    Update Day24CodeShare.sql

--- Day24CodeShare.sql
+++ Day24CodeShare.sql
@@ -1,1 +1,19 @@
+--general example 
+
+BEGIN TRY
+
+    SELECT 1/0 AS Error
+
+END TRY
+BEGIN CATCH
+
+    SELECT
+        ERROR_NUMBER() AS ErrorNumber,
+        ERROR_STATE() AS ErrorState,
+        ERROR_SEVERITY() AS ErrorSeverity,
+        ERROR_LINE() AS ErrorLine,
+        ERROR_PROCEDURE() AS ErrorProcedure,
+        ERROR_MESSAGE() AS ErrorMessage
+
+END CATCH
 
