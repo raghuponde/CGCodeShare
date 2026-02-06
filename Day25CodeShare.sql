0efb454bfad970754b16ec3af8c652f28779c384
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-02-06 10:21:29 +0530

    Update Day25CodeShare.sql

--- Day25CodeShare.sql
+++ Day25CodeShare.sql
@@ -1,1 +1,43 @@
 
+-Views demo 
+--______________
+--views mainly used for security reasons see i have created a table some parts of information 
+--i dont want my junior or subordinate to see so i will created a view for him and second 
+--reason is i had written a complex query involving joins and i want to store it for 
+--future use then also i convert it into view 
+
+ 
+--A view is a virtual table whose contents are defined by a query.
+--Like a real table a view consists of a set of named columns and 
+--rows of the data.
+--The view is not stored in data base if   i am creating a table 
+--it will be stored in database but view is actually derived from
+--a some other table.The rows and columns of data come from the tables referenced
+--in the query defining the view,
+--it acts as a filter.it is stored as an object in the database,which does not 
+--have its own data.A view will derive its data from one or more table
+
+
+--explain them graphically ..okay ..drop it in editor and apply with encryption also for the
+--view and then try doing schema binding and try to do edit operation on main 
+--tables then it gives u nice warning message okay .so if i say yes in this message 
+--then schema binding will be removed ..
+
+
+
+--In SQL Server, views can update the underlying base tables if they are “updatable” 
+--(simple joins, no aggregates, etc.), and schema binding (WITH SCHEMABINDING)
+--locks the view to the schema of its base tables so you cannot change those tables 
+--in ways that would break the view.
+
+--Below are Northwind‑specific examples using multiple tables, 
+--plus explanations of schema binding and other view features.
+
+--1. Simple multi‑table view (Orders + Customers)
+--Assume you have:
+
+--Customers (CustomerID, CompanyName, City)
+
+--Orders (OrderID, CustomerID, OrderDate, Freight)
+
+--Create a view:
