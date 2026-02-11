1de890f542cea9203b518d76ef9e56125d444f71
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-02-11 10:53:11 +0530

    Update Day27CodeShare.cs

--- Day27CodeShare.cs
+++ Day27CodeShare.cs
@@ -57,17 +57,35 @@ namespace LinkedList
             }
 
         }
+        public void addFirst(int e)
+        {
+            Node newest = new Node(e, null);
+            if(isEmpty())
+            {
+                head = newest;
+                tail = newest;
+
+            }
+            else
+            {
+                newest.next = head;
+                head = newest;
+            }
+            size = size + 1;
+        }
         static void Main(string[] args)
         {
             Program l=new Program();
             l.addLast(10);
             l.addLast(20);
             l.addLast(13);
-            Console.WriteLine("Size: " + l.length());
+            Console.WriteLine("\nSize: " + l.length());
             l.addLast(67);
             l.addLast(8);
             l.Display();
-            Console.WriteLine("Size: " + l.length());
+            Console.WriteLine("\nSize: " + l.length());
+            l.addFirst(100);
+            l.Display();
             Console.ReadLine();
         }
     }
