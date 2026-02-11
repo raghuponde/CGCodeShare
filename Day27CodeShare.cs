da58ccbe415a7ea78a56356b83f5cbc19a1fe83c
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-02-11 10:37:00 +0530

    Update Day27CodeShare.cs

--- Day27CodeShare.cs
+++ Day27CodeShare.cs
@@ -1,1 +1,31 @@
 
+namespace LinkedList
+{
+    public class Node
+    {
+        public int element;
+        public Node next;
+        public Node(int e ,Node n)
+        {
+                element = e;
+                next = n;
+        }
+    }
+     class Program
+    {
+        private Node head;
+        private Node tail;
+        private int size;
+
+        public Program()
+        {
+            head = null;
+            tail = null;
+            size = 0;
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
