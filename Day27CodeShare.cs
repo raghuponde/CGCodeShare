ac5ba94291f50bde03f04346a8ec613906331f72
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-02-11 12:02:28 +0530

    Update Day27CodeShare.cs

--- Day27CodeShare.cs
+++ Day27CodeShare.cs
@@ -585,3 +585,213 @@ namespace LinkedList
         }
     }
 }
+try to modify any where delete code 
+-------------------------------------
+namespace LinkedList
+{
+    public class Node
+    {
+        public int element;
+        public Node next;
+        public Node(int e ,Node n)
+        {
+                element = e;
+                next = n;
+        }
+    }
+     class Program
+    {
+        private Node head;
+        private Node tail;
+        private int size;
+
+        public Program()
+        {
+            head = null;
+            tail = null;
+            size = 0;
+        }
+        public int length()
+        {
+           return size;
+        }
+        public bool isEmpty()
+        {
+            return size == 0;
+        }
+
+        public void addLast(int e)
+        {
+            Node newest = new Node(e, null);
+            if(isEmpty())
+            {
+                head=newest;
+            }
+            else
+            {
+                   tail.next=newest;
+            }
+            tail=newest;
+            size=size+1;
+        }
+        public void Display()
+        {
+            Node p = head;
+            while (p!=null)
+            {
+                Console.Write(p.element+"----> ");
+                p=p.next;
+
+            }
+            Console.WriteLine();
+
+        }
+        public void addFirst(int e)
+        {
+            Node newest = new Node(e, null);
+            if(isEmpty())
+            {
+                head = newest;
+                tail = newest;
+
+            }
+            else
+            {
+                newest.next = head;
+                head = newest;
+            }
+            size = size + 1;
+        }
+
+        public void AddAny(int e,int position)
+        {
+            if(position< 0 || position > size)
+            {
+                Console.WriteLine("Invalid postion");
+                return;
+            }
+            if (position == 0)
+            {
+                addFirst(e);
+                return;
+            }
+
+            if (position == size)
+            {
+                addLast(e);
+                return;
+            }
+            Node newest = new Node(e,null);
+            
+            Node p = head;
+            int i = 1;
+            while(i < position -1)
+            {
+                p = p.next;
+                i = i + 1;
+            }
+            newest.next = p.next;
+            p.next = newest;
+            size = size + 1;
+        }
+        public int removeFirst()
+        {
+            if(isEmpty())
+            {
+                Console.WriteLine("List is empty");
+                return -1;
+            }
+            
+            
+                int e = head.element;
+                head = head.next;
+                size = size - 1;
+                if(isEmpty())
+                {
+                    tail = null;
+                }
+                return e;
+
+            
+            
+        }
+        public int removeLast()
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("List is empty");
+                return -1;
+            }
+
+            int e = tail.element;
+            if (size == 1)
+            {
+                head = null;
+                tail = null;
+            }
+            else
+            {
+                Node p = head;
+                while (p.next != tail)
+                {
+                    p = p.next;
+                }
+
+                p.next = null;
+                tail = p;
+            }
+
+            size = size - 1;
+            return e;
+        }
+        public int removeAny(int pos)
+        {
+            if((pos < 0)||(pos >= size-1))
+            {
+                Console.WriteLine("Invalid postion");
+                return -1;
+            }
+            Node p = head;
+            int i = 1;
+            while(i<pos-1)
+            {
+                p = p.next;
+                i = i + 1;
+            }
+            int e = p.next.element;
+            p.next = p.next.next;
+            size = size - 1;
+            return e;
+        }
+        static void Main(string[] args)
+        {
+            Program l=new Program();
+            l.addLast(10);
+            l.addLast(20);
+            l.addLast(13);
+            Console.WriteLine("\nSize: " + l.length());
+            l.addLast(67);
+            l.addLast(8);
+            l.Display();
+            Console.WriteLine("\nSize: " + l.length());
+            l.addFirst(100);
+            l.Display();
+            l.AddAny(39,0);
+            l.Display();
+           int element= l.removeFirst();
+            Console.WriteLine($"\n removed elememt {element}:");
+            l.Display();
+            Console.WriteLine("\nSize: " + l.length());
+            int element2 = l.removeLast();
+            Console.WriteLine($"\n removed elememt {element2}:");
+            l.Display();
+            Console.WriteLine("\nSize: " + l.length());
+            int element3 = l.removeAny(2);
+            Console.WriteLine($"\n removed elememt {element3}:");
+            l.Display();
+            Console.WriteLine("\nSize: " + l.length());
+            Console.ReadLine();
+        }
+    }
+}
+
