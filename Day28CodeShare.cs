8b549feeba1ca42147c4f1e68a91bc28171e58a5
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-02-12 09:55:45 +0530

    Update Day28CodeShare.cs

--- Day28CodeShare.cs
+++ Day28CodeShare.cs
@@ -1,1 +1,60 @@
 
+namespace StacksUsingArrays
+{
+    class StackArray
+    {
+        int[] data;
+
+        int top;
+
+        public StackArray(int n)
+        {
+               data=new int[n];
+              top = -1;
+        }
+        public int length()
+        {
+            return top+1;
+        }
+        public bool isEmpty()
+        {
+            return top == 0;
+        }   
+        public bool isFull()
+        {
+            return top == data.Length;
+        }
+
+        public void push(int e)
+        {
+            if(isFull())
+            {
+                Console.WriteLine("Stack is full");
+                return;
+
+            }
+            data[top] = e;
+            top = top + 1;
+        }
+        public int pop()
+        {
+            if(isEmpty())
+            {
+                Console.WriteLine("Stack is empty");
+                return -1;
+            }
+            int e= data[top];
+            top = top - 1;
+            return e;
+
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
