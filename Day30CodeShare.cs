32aa8ede0542f49ebc256bb1de59cf2791fe1f67
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-02-16 09:53:48 +0530

    Update Day30CodeShare.cs

--- Day30CodeShare.cs
+++ Day30CodeShare.cs
@@ -8,3 +8,51 @@ Assertions: Assertions are used to verify that the expected output matches the a
 Test Suites: A collection of related test cases.
 Test Runner: A tool or part of the framework that executes the tests and provides feedback on whether they passed or failed.
 
+open one class library project and write the below code 
+
+namespace ArthematicOpsandAnother
+{
+    public class Calculate 
+    {
+        public int Addition(int num1,int num2)
+        {
+            return num1 + num2;
+        }
+        public int substract(int num1,int num2)
+        {
+            int result;
+            if(num1>num2)
+            {
+                result = num1 - num2;
+            }
+            else
+            {
+                result = num2 - num1;
+            }
+            return result;
+        }
+        public int Multiplication(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public int Divide(int num1, int num2)
+        {
+            int result = 0;
+            try
+            {
+                result = num1 / num2;
+            }
+            catch (DivideByZeroException ex)
+            {
+                throw ex;
+            }
+            return result;
+
+
+        }
+
+
+    }
+}
+
