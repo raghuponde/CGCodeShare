11caf24b6878b130e5dad17c3d57e5cfccd4d8f8
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-02-16 10:26:10 +0530

    Update Day30CodeShare.cs

--- Day30CodeShare.cs
+++ Day30CodeShare.cs
@@ -99,5 +99,31 @@ namespace ArthematicOpsandAnother
 Now add one test project 
 i am adding NUNIt test project to test the abve code whih i had written i need to add dll refercne of above into the test project
 
+using ArthematicOpsandAnother;
+namespace TestProject1
+{
+    [TestFixture]
+    public class Tests
+    {
+        Calculate cal = null;
+        [SetUp]
+        public void Setup()
+        {
+            cal = new Calculate();
+        }
 
+        [Test]
+        public void Addition()
+        {
+            //arrange 
+            int actual=30;
+            int expected=40;
+            //act 
+            actual = cal.Addition(10,20);
+            //assert
+            Assert.AreEqual(expected, actual);
+            Assert.Pass();
+        }
+    }
+}
 
