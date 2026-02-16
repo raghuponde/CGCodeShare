ddd44babb8031cc3bdfc64fd43902228a95f6230
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-02-16 11:07:39 +0530

    Update Day30CodeShare.cs

--- Day30CodeShare.cs
+++ Day30CodeShare.cs
@@ -72,7 +72,7 @@ namespace ArthematicOpsandAnother
             {
                 result = result + 1;
             }
-
+ 
             string specialchars = @"%!@#$%^&*()?/>.<,:;'\|}]{[_~`+=-" + "\"";
             char[] specarrray = specialchars.ToCharArray();
             foreach (char c in specarrray)
@@ -100,6 +100,11 @@ Now add one test project
 i am adding NUNIt test project to test the abve code whih i had written i need to add dll refercne of above into the test project
 
 using ArthematicOpsandAnother;
+
+
+
+
+
 namespace TestProject1
 {
     [TestFixture]
@@ -114,21 +119,21 @@ namespace TestProject1
 
         [Test]
 
-        [TestCase(15,20)]
-        [TestCase(20, 20)]
+        [TestCase(15,20,35)]
+        [TestCase(20, 20,40)]
 
-        public void Addition(int x,int y)
+        public void Addition(int x,int y,int expected)
         {
             //arrange 
             int actual;
-            int expected=40;
+         //   int expected;
             //act 
             actual = cal.Addition(x,y);
             //assert
             Assert.AreEqual(expected, actual);
             Assert.Pass();
         }
-        [TestCase(20, 15)]
+       // [TestCase(20, 15)]
         [TestCase(20, 20)]
 
         public void Substraction(int x, int y)
@@ -141,6 +146,68 @@ namespace TestProject1
             //assert
             Assert.AreEqual(expected, actual);
             Assert.Pass();
+        }
+
+      //  [TestCase(20, 15)]
+        [TestCase(20, 20)]
+
+        public void multiply(int x, int y)
+        {
+            //arrange 
+            int actual;
+            int expected = 400;
+            //act 
+            actual = cal.Multiplication(x, y);
+            //assert
+            Assert.AreEqual(expected, actual);
+            Assert.Pass();
+        }
+
+       // [TestCase(20, 15)]
+        [TestCase(20, 20)]
+
+        public void Division(int x, int y)
+        {
+            //arrange 
+            int actual;
+            int expected = 0;
+            //act 
+            actual = cal.Divide(x, y);
+            //assert
+            Assert.AreEqual(expected, actual);
+            Assert.Pass();
+        }
+       
+        [Test]
+       public void DivideWithException()
+        {
+            Assert.Throws<DivideByZeroException>(() => cal.Divide(12, 0));
+        }
+
+        [Test]
+         [Ignore("will test it later ")]
+        public void Divide()
+        {
+            int actual = cal.Divide(12, 4);
+            int expected = 3;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("", 0)] // Empty password
+        [TestCase("12345", 1)] // Digits only
+        [TestCase("password123", 2)] // Lowercase + digits
+        [TestCase("Password123", 3)] // Uppercase + lowercase + digits
+        [TestCase("Password@123", 4)] // Special char + uppercase + lowercase + digits
+        public void GetPasswordStrength_ShouldReturnExpectedStrength(string password, int expectedStrength)
+        {
+            int result = cal.GetPassWordStrength(password);
+            Assert.AreEqual(expectedStrength, result);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            cal = null;
         }
 
     }
