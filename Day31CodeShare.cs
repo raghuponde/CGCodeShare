8a0c238e67ec9a13046a3787136f70596e8fbbeb
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-02-17 09:52:09 +0530

    Update Day31CodeShare.cs

--- Day31CodeShare.cs
+++ Day31CodeShare.cs
@@ -11,3 +11,37 @@ Scenario: Implement a BankAccount class that supports deposits, withdrawals, and
 
 Step 1: Write the Failing Test First (RED)
 Create the failing test before writing the BankAccount class. The test will fail because the class and methods don’t exist yet.
+
+
+  namespace TDDDemo
+{
+    [TestFixture]
+    public class BankAccountTests
+    {
+        [Test]
+        public void Deposit_shouldIncrease()
+        {
+            var account = new BankAccount();
+            account.Deposit(100);
+            Assert.AreEqual(100, account.Balance);
+        }
+
+        [Test]
+        public void Withdraw_shouldDecrease()
+        {
+            var account = new BankAccount();
+            account.Deposit(100);
+            account.Withdraw(50);
+            Assert.AreEqual(50, account.Balance);
+        }
+        [Test]
+        public void Withdraw_shoulldThrowException_insufficent()
+        {
+            var account = new BankAccount();
+           
+            account.Withdraw(50);
+            Assert.Throws<InvalidOperationException>(() => account.Withdraw(100));
+        }
+
+    }
+}
