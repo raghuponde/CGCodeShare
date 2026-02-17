eaee9ac4ce8120ac1e241214a24adaa1532409f6
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-02-17 11:53:48 +0530

    Update Day31CodeShare.cs

--- Day31CodeShare.cs
+++ Day31CodeShare.cs
@@ -189,7 +189,7 @@ public class Calculator
 {
     public int Divide(int numerator, int denominator)
     {
-        if (denominator == 0)
+        if (denominator == 0) 
         {
             throw new DivideByZeroException("Denominator cannot be zero.");
         }
