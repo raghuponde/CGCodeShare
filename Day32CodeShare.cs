6cbc782f6137db3f219bcea961c2f298d28a08a1
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-02-18 09:51:00 +0530

    Update Day32CodeShare.cs

--- Day32CodeShare.cs
+++ Day32CodeShare.cs
@@ -1,1 +1,19 @@
 
+namespace IEnumerableDemo
+{
+
+    class Employee
+    {
+        public int ID { set; get; }
+        public string Name { set; get; }
+
+        public DateTime HireDate { set; get; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
