0c4f455ecccc2c7627335a6395fabaa4a32926ba
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-02-18 11:01:34 +0530

    Update Day32CodeShare.cs

--- Day32CodeShare.cs
+++ Day32CodeShare.cs
@@ -362,3 +362,56 @@ foreach (var kvp in dict) Console.WriteLine($"{kvp.Key}: {kvp.Value}");  // Work
 
 string text = "Hello";
 foreach (char c in text) Console.WriteLine(c);  // Works! (chars)
+
+next program
+-------------
+namespace IEnumerableDemo2
+{
+    class Employee
+    {
+        public string Name { set; get; }
+        public int Salary { set; get; }
+        public int DepartmentId { set; get; }
+
+        public Employee(string name1,int salary1,int deptid)
+        {
+            Name = name1;
+            Salary = salary1;
+            DepartmentId = deptid;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3 };
+            foreach (int n in numbers)
+            {
+                Console.WriteLine(n);
+            }// Works!
+
+            List<string> names = new List<string> { "Alice", "Bob" };
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }// Works!
+
+            Dictionary<int, string> dict = new Dictionary<int, string> { { 1, "One" } };
+            foreach (var kvp in dict)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value}")
+                    ;  // Works!
+            }
+
+            string text = "Hello";
+            foreach (char c in text)
+            {
+                Console.WriteLine(c);
+            }// Works! (chars)
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
