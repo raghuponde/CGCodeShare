29579adb46f71f3227ce716d412c667a90f6f03d
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-02-18 09:58:21 +0530

    Update Day32CodeShare.cs

--- Day32CodeShare.cs
+++ Day32CodeShare.cs
@@ -32,3 +32,43 @@ namespace IEnumerableDemo
 in the above i want to implment filtering means i want to get emplyees which hired befoe 2005
 
     
+namespace IEnumerableDemo
+{
+
+    class Employee
+    {
+        public int ID { set; get; }
+        public string Name { set; get; }
+
+        public DateTime HireDate { set; get; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Employee> employees = new List<Employee>()
+            {
+                new Employee{ID=1,Name="Raghu" ,HireDate=new DateTime(2002,3,5)},
+                new Employee{ID=2,Name="Mohan" ,HireDate=new DateTime(2001,3,5)},
+                new Employee{ID=3,Name="Kiran" ,HireDate=new DateTime(2007,3,5)},
+
+            };
+
+            IEnumerable<Employee> query = from e in employees
+                                          where e.HireDate.Year < 2005
+                                          orderby e.Name
+                                          select e;
+
+            //foreach(Employee e in employees)
+            //{
+            //    Console.WriteLine($"{e.Name}");
+            //}
+
+            foreach (Employee e in query)
+            {
+                Console.WriteLine($"{e.Name}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
