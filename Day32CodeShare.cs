db7ade7634fd25a201f18ac124a021f03881fdb0
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-02-18 09:55:27 +0530

    Update Day32CodeShare.cs

--- Day32CodeShare.cs
+++ Day32CodeShare.cs
@@ -1,4 +1,3 @@
-
 namespace IEnumerableDemo
 {
 
@@ -13,7 +12,19 @@ namespace IEnumerableDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            IEnumerable<Employee> employees = new List<Employee>()
+            {
+                new Employee{ID=1,Name="Raghu" ,HireDate=new DateTime(2002,3,5)},
+                new Employee{ID=2,Name="Mohan" ,HireDate=new DateTime(2001,3,5)},
+                new Employee{ID=3,Name="Kiran" ,HireDate=new DateTime(2007,3,5)},
+
+            };
+
+            foreach(Employee e in employees)
+            {
+                Console.WriteLine($"{e.Name}");
+            }
+            Console.ReadLine();
         }
     }
 }
