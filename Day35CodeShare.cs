949dba5b26ef3b87539b31c699aa38abe8a5393d
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-02-23 10:50:58 +0530

    Update Day35CodeShare.cs

--- Day35CodeShare.cs
+++ Day35CodeShare.cs
@@ -1,1 +1,51 @@
 
+namespace TreeDemo
+{
+
+    class Node
+    {
+        public int element;
+        public Node left;
+        public Node right;
+
+        public Node(int e,Node l,Node r)
+        {
+            element = e;
+            left = l;
+            right = r;
+        }
+    }
+
+    class BinarySearchTree
+    {
+        Node root;
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+
+        public void insert(Node temproot,int e)
+        {
+            Node temp = null;
+            while(temproot!=null)
+            {
+                temp = temproot;
+                if (e == temproot.element)
+                    return;
+                else if (e < temproot.element)
+                    temproot = temproot.left;
+                else if (e > temproot.element)
+                    temproot = temproot.right;
+
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
