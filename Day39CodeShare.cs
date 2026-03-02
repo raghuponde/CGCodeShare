dd98d6d70602e4dfa0e60575fc35db8ce4969ab4
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-03-02 09:40:21 +0530

    Update Day39CodeShare.cs

--- Day39CodeShare.cs
+++ Day39CodeShare.cs
@@ -1,1 +1,9 @@
 
+Step 1: Create Console Project
+In Visual Studio 2026, create new Console App targeting .NET 8.0, name it "CodeFirstDemo".
+Install NuGet packages:
+
+Microsoft.EntityFrameworkCore (8.0.24)
+Microsoft.EntityFrameworkCore.SqlServer (8.0.24)
+Microsoft.EntityFrameworkCore.Tools (8.0.24)
+
