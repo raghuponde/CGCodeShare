539b89472464b77df37ecfc750de1d5d858fb0ea
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-03-03 11:03:19 +0530

    Update Day40CodeShare.cs

--- Day40CodeShare.cs
+++ Day40CodeShare.cs
@@ -145,3 +145,48 @@ IProductRepository obj = new ProductRepository(context);
 //}
 
 await obj.DeleteAsync(7);
+
+
+Now i want to do the same above tasks using stored procedures interface will not change only thing is that
+in ProductRepository2  i will call stored procedures and remember u have to create stored procedures in 
+cgefdb1 db only 
+
+so write the below stored procedures and execute them one by one 
+
+-- GetAllProducts
+CREATE OR ALTER PROCEDURE GetAllProducts
+AS
+BEGIN
+    SELECT Id, Name, Price, CategoryId FROM Products;
+END
+
+-- GetProductById
+CREATE OR ALTER PROCEDURE GetProductById @Id int
+AS
+BEGIN
+    SELECT Id, Name, Price, CategoryId FROM Products WHERE Id = @Id;
+END
+
+-- InsertProduct
+CREATE OR ALTER PROCEDURE InsertProduct @Name nvarchar(100), @Price decimal(18,2), @CategoryId int
+AS
+BEGIN
+    INSERT INTO Products (Name, Price, CategoryId) OUTPUT INSERTED.Id, INSERTED.Name, INSERTED.Price, INSERTED.CategoryId
+    VALUES (@Name, @Price, @CategoryId);
+END
+
+-- UpdateProduct
+CREATE OR ALTER PROCEDURE UpdateProduct @Id int, @Name nvarchar(100), @Price decimal(18,2), @CategoryId int
+AS
+BEGIN
+    UPDATE Products SET Name = @Name, Price = @Price, CategoryId = @CategoryId
+    WHERE Id = @Id;
+END
+
+-- DeleteProduct
+CREATE OR ALTER PROCEDURE DeleteProduct @Id int
+AS
+BEGIN
+    DELETE FROM Products WHERE Id = @Id;
+END
+
