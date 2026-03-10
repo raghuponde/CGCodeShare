4958fc3b1d39cf2fcaa195fe2f0120340e52fd8c
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-03-10 10:34:24 +0530

    Update Day44CodeShare.js

--- Day44CodeShare.js
+++ Day44CodeShare.js
@@ -229,9 +229,26 @@ let sum2 = (n1, n2) => (n1 + n2);
 testme();
 console.log(sum2(12, 56));
 
+//variable.map(element)=>print(element)
 
-Map function demo 
---------------------
-Variable.map((element)=> print(element))
+var arr = [10, 20, 30, 40, 50];
+arr.map((ele) => console.log(ele));
 
+// example 2 
+const numbers = [1, 2, 3, 4, 5];
+const squares = numbers.map(value => value * value);
+console.log(squares);
 
+const people = [{ id: 1, name: "felpie", country: "USA" },
+{ id: 2, name: "mohan", country: "INDIA" },
+{ id: 3, name: "jagdish", country: "SRILanka" }
+]
+
+const names = people.map(p => p.name);
+console.log(names);
+
+// filter function
+//array.filter(ele=>(condition))
+
+var filtered = numbers.filter(x => x > 3);
+console.log(filtered);
