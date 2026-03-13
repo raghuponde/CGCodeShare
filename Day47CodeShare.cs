b2795d069416a802b371d60b6db166f440aff150
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-03-13 09:07:30 +0530

    Update Day47CodeShare.cs

--- Day47CodeShare.cs
+++ Day47CodeShare.cs
@@ -1,1 +1,13 @@
 
+CRUD operation using a Model :
+-----------------------------------------
+Means for a model means for a class any class i want to define insert,update ,delete and read etc methods in Controller function means function for insert and function for update and all i want to write 
+here i will use get and post methods and all i will validate the class means business rules also i will apply 
+
+earlier manually i had gone into the view and written html code and embedded the model object s into it 
+but now i will ask the visual studio to genrate the code for me in the view that thing is called as scafffolding.
+
+so check program dog on this example ..
+
+
+First add a model class Dog here 
