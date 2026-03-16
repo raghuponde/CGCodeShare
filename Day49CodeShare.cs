a7cfaa4218c678704361ceda2e3d1cdebb752cc8
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-03-16 10:04:15 +0530

    Update Day49CodeShare.cs

--- Day49CodeShare.cs
+++ Day49CodeShare.cs
@@ -14,7 +14,9 @@ DbFirst approach of Entity Framework in asp.net core mvc
 
 --->then in package manager console fire this command 
 
-Scaffold-DbContext "Server=LAPTOP-4G8BHPK9\SQLEXPRESS;Database=NorthWind;Trusted_Connection=True;TrustServerCertificate=True;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models -Context NorthWindContext -Tables CustomProducts -Force
+
+Scaffold-DbContext "Server=LAPTOP-4G8BHPK9\SQLEXPRESS;Database=NorthWind;Trusted_Connection=True;TrustServerCertificate=True;
+Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models -Context NorthWindContext -Tables CustomProducts -Force"
 
 change above as per your server and as per your db okay 
 
