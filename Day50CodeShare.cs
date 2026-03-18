919bbb60f9097c3684447412b514b8f9ebe2154f
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-03-18 09:41:43 +0530

    Update Day50CodeShare.cs

--- Day50CodeShare.cs
+++ Day50CodeShare.cs
@@ -1,1 +1,10 @@
 
+Add one class with the name ProdCat in Models folder 
+
+
+    public class ProdCat
+    {
+        public string? prodname { set; get; }
+        public string? catname { set; get; }
+    }
+
