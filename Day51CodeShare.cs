86e337aa8393dac3ff7bcf556abb1fbd81de25f1
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-03-19 10:44:00 +0530

    Update Day51CodeShare.cs

--- Day51CodeShare.cs
+++ Day51CodeShare.cs
@@ -169,4 +169,76 @@ namespace CodeFirstEFInAsp.netcoreDemo.Models
      }
 }
 
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeFirstEFInAsp.netcoreDemo.Models
+{
+    public class Employee
+    {
+        public int Id { set; get; }
+
+        [Required(ErrorMessage ="please enter your first name ")]
+        public string FirstName { set; get; }
+
+        [Required(ErrorMessage = "please enter your last name ")]
+        public string LastName { set; get; }
+
+        [Required(ErrorMessage = "please enter your email  ")]
+        [EmailAddress(ErrorMessage ="enter valid email")]
+        public string Email { set; get; }
+
+        [Required(ErrorMessage="enter your age ")]
+        [Range(0,100,ErrorMessage ="please enter age between 1 to 100 only ")]
+        public int Age { set; get; }
+
+    }
+}
+
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeFirstEFInAsp.netcoreDemo.Models
+{
+    public class UserDetail
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "User Name is Required")]
+        [StringLength(15, ErrorMessage = "User Name cannot be more than 15 characters")]
+        public string? UserName { get; set; }
+
+        [Required(ErrorMessage = "Password Required")]
+        [StringLength(11, MinimumLength = 5, ErrorMessage = "Minimum Length of Password is 5 letters or Max Length is of 11 letters..")]
+        [DataType("password")]
+        public string? NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [System.ComponentModel.DataAnnotations.Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string? ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Date Of Birth is Required")]
+        [DisplayName("Date of Birth")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = "Email is Required")]
+        [EmailAddress(ErrorMessage = "Please enter valid Email Id")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Postal Code is Required")]
+        [Range(100, 1000, ErrorMessage = "Must be between 100 and 1000")]
+        public int PostalCode { get; set; }
+
+        [Required(ErrorMessage = "Phone Number is Required")]
+        [DisplayName("Phone Number")]
+        public int PhoneNo { get; set; }
+
+        [Required(ErrorMessage = "Profile is Required")]
+        [DataType(DataType.MultilineText)]
+        public string Profile { get; set; }
+    }
+}
 
