af2d43a7ee8998ff3aab576d7196377be7f0c354
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-03-19 10:33:15 +0530

    Update Day51CodeShare.cs

--- Day51CodeShare.cs
+++ Day51CodeShare.cs
@@ -129,5 +129,44 @@ which will provide valdiation to me accordingly constraints will be imposed on t
  
  so now add classes Author1 ,Course1 ,Employee ,UserDetail into the models folder 
  
+namespace CodeFirstEFInAsp.netcoreDemo.Models
+{
+    public class Author1
+    {
+        public string Id { set; get; }
+        public string Name { get; set; }
+
+        public IList<Course1> Courses { set; get; }
+    }
+}
+
+
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeFirstEFInAsp.netcoreDemo.Models
+{
+    public class Course1
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { set; get; }
+
+        [Required]
+        [Column("Stitle",TypeName ="varchar")]
+        public string Title { set; get; }
+
+        [Required]
+        [MaxLength(220)]
+        public string Description { set; get; }
+
+        public float fullprice { set; get; }
+
+        public Author1 Author { set; get; }
+
+        [ForeignKey("Author")]
+        public int AuthorId { set; get; }
+     }
+}
 
 
