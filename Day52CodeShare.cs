5d32e9400eb4edee234c07aed2a523998d52ce02
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-03-20 10:13:50 +0530

    Update Day52CodeShare.cs

--- Day52CodeShare.cs
+++ Day52CodeShare.cs
@@ -16,3 +16,111 @@ inside it and later on partial view which will take some model object than
 
 and always create this partial view in shared folder because some other controller class can also use it as per its requiremnt 
 
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PartialViewCGDemo.Models;
+
+namespace PartialViewCGDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            return View();
+        }
+        Employee emp = new Employee()
+        {
+           EmpId=101,
+           EmpName="ravi",
+           Email="ravi@gmail.com",
+           Description= "ravi is good working employeeravi" +
+            " is good working employeeravi is good working " +
+            "employeeravi is good working employeeravi" +
+            " is good working employeeravi is good working " +
+            "employeeravi is good working employee"
+        
+        };
+        public IActionResult displayemp()
+        {
+
+            return View(emp);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+
+
+------------------------
+namespace PartialViewCGDemo.Models
+{
+    public class Employee
+    {
+        public int EmpId { set; get; }
+        public string? EmpName { set; get; }
+        public string? Email { set; get; }
+        public string? Description { set; get; }
+    }
+}
+--------------
+_card 
+---------
+@model int 
+
+<h2>Hello world -->@Model</h2>
+
+
+----index 2 view 
+
+@{
+    ViewData["Title"] = "Index2";
+    int n = 10;
+}
+
+<h1>Index2</h1>
+
+@{
+    for(int i=0;i<=n;i++)
+    {
+       @Html.Partial("_Card",@i)
+    }
+}
+
+-----------display emp view ----
+                    @model PartialViewCGDemo.Models.Employee
+@{
+    ViewData["Title"] = "displayemp";
+}
+
+<h1>displayemp</h1>
+<div class="row" style="border:solid 3px">
+
+    <div class="col-lg-6">
+        <h3>Name:@Model.EmpName</h3>
+        <p>ID:@Model.EmpId</p>
+        <p>Email:@Model.Email</p>
+    </div>
+    <div class="col-lg-6">
+        <h3>Description</h3>
+        <p>@Model.Description</p>
+    </div>
+</div>
