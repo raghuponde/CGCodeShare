1e6d7e32ea72aaa65a370f11ae09721e0b8a9ce7
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-03-23 09:53:36 +0530

    Update Day53CodeShare.cs

--- Day53CodeShare.cs
+++ Day53CodeShare.cs
@@ -1,1 +1,10 @@
 
+  <form asp-action="SetA" method="post">
+        <button type="submit" class="btn btn-primary">Set A to 10</button>
+    </form>
+
+    <form asp-action="GetA" method="post">
+        <button type="submit" class="btn btn-secondary">Get A Value</button>
+    </form>
+
+    <p>Value of A: @ViewBag.AValue</p>
