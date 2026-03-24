f9426b64d00442678d99b7f0a3995201fcedb544
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-03-24 09:54:38 +0530

    Update Day54CodeShare.cs

--- Day54CodeShare.cs
+++ Day54CodeShare.cs
@@ -35,4 +35,73 @@ create one folder in day54 with name jquerydemos2 and add one html file into it 
 </body>
 </html>
 
+updated code 
+--------------
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js">
+
+    </script>
+    <script type="text/javascript" language="Javascript">
+        var friends = ["sunitha", "mahesh", "kiran", "sanmay"];
+
+        var objs = [{ "name": "sita", "age": "23" }, { "name": "rajesh", "age": "55" }, { "name": "kiran", "age": "22" },
+        { "name": "praveen", "age": "27" }]
+
+        $(document).ready(
+
+            function () {
+
+                $.each(friends, function (index, value) {
+                    console.log((index + 1) + ":----->" + value);
+
+                })
+
+                $.each(objs, function (index, value) {
+                    console.log((index + 1) + ":" + value.name + " is having age " + value.age);
+                })
+
+                console.log("I want to iterate list")
+                $("li").each(function(index,element){
+                    alert((index+1)+ ": "+$(element).text())
+                })
+
+                console.log("I want to iterate using reference")
+
+                $("li").each(function(index){
+                    alert((index+1)+":"$(this).text());
+                })
+
+            }
+
+
+
+
+
+
+        );
+    </script>
+</head>
+
+<body>
+    <ul>
+        <li>US</li>
+        <li>India</li>
+        <li>UK</li>
+        <li>Canada</li>
+        <li>Australia</li>
+    </ul>
+
+    <div id="divresult" style="background-color: yellow;">
+
+
+
+    </div>
+</body>
+
+</html>
