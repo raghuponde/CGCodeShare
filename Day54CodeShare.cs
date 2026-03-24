ae770d16de541ac7b930ac4384c1e74597c6dc08
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-03-24 09:29:27 +0530

    Update Day54CodeShare.cs

--- Day54CodeShare.cs
+++ Day54CodeShare.cs
@@ -1,1 +1,38 @@
+create one folder in day54 with name jquerydemos2 and add one html file into it with name dollareachdemos.html and paste the below code into it 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js">
+
+    </script>
+    <script type="text/javascript" language="Javascript">
+         var friends = ["sunitha", "mahesh", "kiran", "sanmay"];
+
+            var objs = [{ "name": "sita", "age": "23" }, { "name": "rajesh", "age": "55" }, { "name": "kiran", "age": "22" },
+            { "name": "praveen", "age": "27" }]
+
+            $(document).ready();
+        </script>
+</head>
+<body>
+    <ul>
+        <li>US</li>
+        <li>India</li>
+        <li>UK</li>
+        <li>Canada</li>
+        <li>Australia</li>
+    </ul>
+    
+    <div id="divresult" style="background-color: yellow;">
+    
+     
+    
+    </div> 
+</body>
+</html>
+
 
