62511b921db2bf27a7e9d5b616581c51d83e86ea
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-03-24 11:05:00 +0530

    Update Day54CodeShare.cs

--- Day54CodeShare.cs
+++ Day54CodeShare.cs
@@ -247,4 +247,30 @@ web api demo
 --------------
 create a new asp.net core mvc project now 
 
+add a new model class in Models folder 
+
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApiInAsp.netcoreMvcDemo.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter your firstname")]
+        public string? FirstName { set; get; }
+
+        [Required(ErrorMessage = "Please enter your lastname")]
+        public string? LastName { set; get; }
+
+        [Required(ErrorMessage = "Please enter email id")]
+        [EmailAddress(ErrorMessage = "Please enter valid email id")]
+        public string? Email { set; get; }
+
+        [Required(ErrorMessage = "Please enter your age")]
+        [Range(0, 100, ErrorMessage = "Please enter your age betwen 1 to 100 only ")]
+
+        public int Age { set; get; }
+    }
+}
 
