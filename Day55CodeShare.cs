b27d4e7cb6943f01f0e2640d60af2eedce9c967f
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-03-25 10:46:06 +0530

    Update Day55CodeShare.cs

--- Day55CodeShare.cs
+++ Day55CodeShare.cs
@@ -288,8 +288,39 @@ To fully implement image upload, pagination, and validation in your ASP.NET Core
 
 ✅ Controller EmployeeController with model validation and pagination
 
-✅ Configure File Upload Folder
+✅ Configure File Upload Folder 
 
 ✅ Dependency Injection in Program.cs
+
+model change 
+-------------
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApiInAsp.netcoreMvcDemo.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter your firstname")]
+        public string? FirstName { set; get; }
+
+        [Required(ErrorMessage = "Please enter your lastname")]
+        public string? LastName { set; get; }
+
+        [Required(ErrorMessage = "Please enter email id")]
+        [EmailAddress(ErrorMessage = "Please enter valid email id")]
+        public string? Email { set; get; }
+
+        [Required(ErrorMessage = "Please enter your age")]
+        [Range(0, 100, ErrorMessage = "Please enter your age betwen 1 to 100 only ")]
+
+        public int Age { set; get; }
+
+        public string? ImagePath { set; get; }
+    }
+}
+
+
 
 
