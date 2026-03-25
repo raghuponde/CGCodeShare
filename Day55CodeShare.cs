1dfff898e60d59075e3d20e83b01115100dda1c1
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-03-25 09:10:41 +0530

    Update Day55CodeShare.cs

--- Day55CodeShare.cs
+++ Day55CodeShare.cs
@@ -1,1 +1,9 @@
 
+you have to put all your code in chat gpt and have to give this prompt but right now dont give this prompt as i had given and taken out code for the above tasks now just keep this prompt 
+with you and use in project differentn stages In the above coding i had written multiple put and post methods so sinlge one only i will take all i will not take 
+
+
+so in the project only add the interface like this and also add class like this 
+
+  
+
