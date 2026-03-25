572598bdfdd846abd4433f8cf5462329149a367a
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-03-25 09:11:05 +0530

    Update Day55CodeShare.cs

--- Day55CodeShare.cs
+++ Day55CodeShare.cs
@@ -2,7 +2,14 @@
 you have to put all your code in chat gpt and have to give this prompt but right now dont give this prompt as i had given and taken out code for the above tasks now just keep this prompt 
 with you and use in project differentn stages In the above coding i had written multiple put and post methods so sinlge one only i will take all i will not take 
 
-
+Prompt:
+    ---------
+    Here i had written the code in web api controller i want it to put it in interface 
+    IEmployee and I want to implement EmployeeService and then i want to use it in 
+    web api controller where i will call a web api controller with read and write actions
+    so provide me the three codes one is IEmployee and then EmployeeService implementing interface and 
+    finally give create a new EmployeeController of web api with read and write actions substituing Employee service
+    
 so in the project only add the interface like this and also add class like this 
 
   
