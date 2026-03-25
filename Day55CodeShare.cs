03ed09678919102d28bd0d6a582050e24e2e0330
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-03-25 10:49:21 +0530

    Update Day55CodeShare.cs

--- Day55CodeShare.cs
+++ Day55CodeShare.cs
@@ -322,5 +322,21 @@ namespace WebApiInAsp.netcoreMvcDemo.Models
 }
 
 
+interface change 
+    ------------
+using Microsoft.AspNetCore.Mvc;
+using WebApiInAsp.netcoreMvcDemo.Models;
+
+namespace WebApiInAsp.netcoreMvcDemo
+{
+    public interface IEmployee
+    {
+        Task<List<Employee>> GetAllEmployeesAsync(int pageNumber,int pageSize);
+        Task<Employee?> GetEmployeeByIdAsync(int id);
+        Task<Employee> AddEmployeeAsync(Employee employee,IFormFile image);
+        Task<Employee?> UpdateEmployeeAsync(Employee employee,IFormFile? image);
+        Task<Employee?> DeleteEmployeeAsync(int id);
+    }
+}
 
 
