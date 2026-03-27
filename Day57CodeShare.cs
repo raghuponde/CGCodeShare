b96622c2aa92914e4a2958c8750fd34efba88aef
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-03-27 11:46:14 +0530

    Update Day57CodeShare.cs

--- Day57CodeShare.cs
+++ Day57CodeShare.cs
@@ -450,3 +450,44 @@ index view
     });
 </script>
 
+
+now in ui controller add further methods 
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiInAsp.netcoreMvcDemo.Controllers
+{
+    public class EmployeeUIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+        public IActionResult Export()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
+
+
