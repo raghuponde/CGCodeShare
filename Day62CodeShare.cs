28e2b120df63046e9186d5ac568da64c90a364af
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-04-06 09:59:46 +0530

    Update Day62CodeShare.cs

--- Day62CodeShare.cs
+++ Day62CodeShare.cs
@@ -6,3 +6,14 @@ and go to app settings and write like this
    "ConnectionStrings": {
     "AzureSqlConnection": ""
   }
+
+
+3) install entity framework packeages of version 8.0.24 packages in total 
+
+4) add data folder and into that dbcontext code and also the model class Person okay 
+
+5) go to program.cs file write this below code and in this what i am doing is that 
+
+
+   var connectionString = builder.Configuration.GetConnectionString("AzureSqlConnection"); // here in appsetting u have to give this value //okay from statement 2 okay 
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
