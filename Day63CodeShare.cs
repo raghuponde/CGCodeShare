59872d6e7c4153fe5186422828f437940aa6506c
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-04-07 11:23:18 +0530

    Update Day63CodeShare.cs

--- Day63CodeShare.cs
+++ Day63CodeShare.cs
@@ -1,1 +1,12 @@
 
+1)First create an asp.net core web api project withname ProductApi 
+
+2)2)and go to app settings and write like this 
+
+   "ConnectionStrings": {
+    "AzureSqlConnection": ""
+  }
+ 
+
+
+
