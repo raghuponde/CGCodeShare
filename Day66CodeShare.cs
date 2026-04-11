dd73c66b9c36127a9ef42915c90167e5ebe0e070
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-04-12 03:34:09 +0530

    Update Day66CodeShare.cs

--- Day66CodeShare.cs
+++ Day66CodeShare.cs
@@ -1,3 +1,10 @@
+create an new mvc application with the name AzureSpookyLogicApp and let the solution name be same only 
+
+and now go Models folder and add this model into it with name SpookyRequest
+
+
+
+
 
 
 
