81eada9b6b98359b71a1a15f98d54c670fb043ed
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-04-13 12:11:39 +0530

    Update Day67CodeShare.cs

--- Day67CodeShare.cs
+++ Day67CodeShare.cs
@@ -105,5 +105,12 @@ below
 
 you have to copy the code same as it is but shoudl should some parameters 
 
-
-
+add this table in azure 
+ public class SpookyRequest
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+     
+    }
