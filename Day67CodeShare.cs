ed207a8901d6a5d5795a2da76390dee13ed6e74c
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-04-13 11:23:02 +0530

    Update Day67CodeShare.cs

--- Day67CodeShare.cs
+++ Day67CodeShare.cs
@@ -1,2 +1,22 @@
 
 create one asp .net core mvc application with name AzureSpookyLoginApp
+
+add one model class into the models folder with the name 
+
+using System.ComponentModel.DataAnnotations;
+
+namespace AzureSpookyLogicApp.Models
+{
+    public class SpookyRequest
+    {
+        public string? Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Phone { get; set; }
+
+    }
+}
+
