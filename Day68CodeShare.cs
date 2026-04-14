960186738f42551fb9ddc484b6181dee1e10b29c
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-04-14 17:23:23 +0530

    Update Day68CodeShare.cs

--- Day68CodeShare.cs
+++ Day68CodeShare.cs
@@ -1,1 +1,12 @@
 
+create an resouce of azure cosmos db with the name raghucosmosdb 
+
+and then go to Data Explorer section and create a database name with DemoDB and by clicking on same database add container inside this database which is nothing but a table here 
+
+items and partion key as id and manual u select and say OK now for creating container okay 
+
+so here id i had taken as partion key and here id is also primary key of table also 
+
+now come to visual studio and open and create asp.net core mvc project now okay ...
+
+  
