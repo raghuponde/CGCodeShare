ceeb963780017628a96e1ec82f0fa3f1ab9dfd64
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-04-15 10:27:53 +0530

    Update Day68CodeShare.cs

--- Day68CodeShare.cs
+++ Day68CodeShare.cs
@@ -5,3 +5,6 @@ Azure key Vault :
 
   
 First create an Azure key vault in azure portal and then you will see there three things keys,secrets and certificates 
+so after creeating key vault and one key and create a dummy app go to console application and create an project with the same name of app 
+
+
