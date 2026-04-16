e60a3d49e1438751d96c4c1c79121c64534104cd
raghuponde <33595609+raghuponde@users.noreply.github.com>
2026-04-16 11:23:37 +0530

    Update Day69CodeShare.cs

--- Day69CodeShare.cs
+++ Day69CodeShare.cs
@@ -17,7 +17,7 @@ create an resouce of azure cosmos db with the name raghucosmosdb
 
 and then go to Data Explorer section and create a database name with DemoDB and by clicking on same database add container inside this database which is nothing but a table here 
 
-items and partion key as id and manual u select and say OK now for creating container okay 
+Items and partion key as id and manual u select and say OK now for creating container okay 
 
 so here id i had taken as partion key and here id is also primary key of table also 
 
